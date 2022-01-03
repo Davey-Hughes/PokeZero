650d2af6d0148f001c52ae5422123e8e1fa9ce89
mingu600 <mingu600@gmail.com>
2022-01-02 22:38:37 -0600

    Normalized vector values, now includes all Pokemon stat values

--- src/main.cc
+++ src/main.cc
@@ -28,12 +28,32 @@
 using json = nlohmann::json;
 
 double normStat(double stat){
-	return (stat - 200.0) / 200.0;
+	return stat / 500 * 2 - 1;
+}
+
+double normBoost(double boost){
+	return (boost + 6) / 6 - 1;
+}
+
+std::vector<double> addNormStats(json boosts, std::vector<double> prev){
+	std::vector<double> tmp, res;
+	tmp.push_back(normBoost(boosts["atk"]));
+	tmp.push_back(normBoost(boosts["def"]));
+	tmp.push_back(normBoost(boosts["spa"]));
+	tmp.push_back(normBoost(boosts["spd"]));
+	tmp.push_back(normBoost(boosts["spe"]));
+	tmp.push_back(normBoost(boosts["accuracy"]));
+	tmp.push_back(normBoost(boosts["evasion"]));
+	res.reserve(prev.size() + tmp.size() ); // preallocate memory
+	res.insert(res.end(), prev.begin(), prev.end() );
+	res.insert(res.end(), tmp.begin(), tmp.end() );
+	return res;
 }
 
 std::vector<double> addNormStats(json stats, int hp, std::vector<double> prev){
 	std::vector<double> tmp, res;
-	tmp.push_back(double(hp) / double(stats["hp"]));
+	tmp.push_back(2 * double(hp) / double(stats["hp"]) - 1);
+	tmp.push_back(normStat(stats["hp"]));
 	tmp.push_back(normStat(stats["atk"]));
 	tmp.push_back(normStat(stats["def"]));
 	tmp.push_back(normStat(stats["spa"]));
@@ -47,7 +67,7 @@ std::vector<double> addNormStats(json stats, int hp, std::vector<double> prev){
 
 std::vector<double> addStatusVector(json a, std::string query, std::vector<double> prev){
 	std::vector<double> res;
-	std::vector<double> a_vec(a.size(), 0);
+	std::vector<double> a_vec(a.size(), -1);
 	a_vec.at(a[query]) = 1;
 	// Concatenate status vector with results vector
 	res.reserve(prev.size() + a_vec.size() ); // preallocate memory
@@ -56,6 +76,18 @@ std::vector<double> addStatusVector(json a, std::string query, std::vector<doubl
 	return res;
 }
 
+std::vector<double> addTypeVector(json a, json types, std::vector<double> prev){
+	std::vector<double> type_vector(18, -1);
+	std::vector<double> res;
+	for(std::string mon_type : a){
+		type_vector.at(types[mon_type]) = 1;
+	}
+	// Concatenate types vector with results vector
+	res.reserve(prev.size() + type_vector.size() ); // preallocate memory
+	res.insert(res.end(), prev.begin(), prev.end() );
+	res.insert(res.end(), type_vector.begin(), type_vector.end() );
+	return res;
+}
 int main()
 {
 	//         return torch.Tensor(ability+item+types+moves+move_pp + move_disabled +status+stats
@@ -75,42 +107,44 @@ int main()
 	ifitems >> items;
 	iftypes >> types;
 	ifstatus >> status;
-	std::vector<double> tmp, tmp2;
-	// Append Pokemon ability
-	std::string ability = state["sides"][0]["pokemon"][0]["ability"];
-	tmp.push_back(abilities[ability]);
-	// Append Pokemon item
-	std::string item = state["sides"][0]["pokemon"][0]["item"];
-	tmp.push_back(items[item]);
-	// Append Pokemon types
-	std::vector<double> type_vector(18, 0);
-	for(std::string mon_type : state["sides"][0]["pokemon"][0]["types"]){
-		type_vector.at(types[mon_type]) = 1;
-	}
-	// Concatenate types vector with results vector
-	tmp2.reserve(tmp.size() + type_vector.size() ); // preallocate memory
-	tmp2.insert(tmp2.end(), tmp.begin(), tmp.end() );
-	tmp2.insert(tmp2.end(), type_vector.begin(), type_vector.end() );
+	std::vector<double> result;
+	for (int side_i = 0; side_i < 2; side_i ++){
+		for (int mon_i = 0; mon_i < int(state["sides"][side_i]["pokemon"].size()); mon_i++){
+			json pokemon = state["sides"][side_i]["pokemon"][mon_i];
+			// Append Pokemon ability
+			std::string ability = pokemon["ability"];
+			result.push_back(abilities[ability]);
+			// Append Pokemon item
+			std::string item = pokemon["item"];
+			result.push_back(items[item]);
+			// Append Pokemon types
+			result = addTypeVector(pokemon["types"], types, result);
 
-	// Append move_id, move pp, move max pp, and move status
-	std::map<int, json> move_map;
-	for (json move: state["sides"][0]["pokemon"][0]["moveSlots"]){
-		std::string move_id = move["id"];
-		move_map.insert(std::make_pair(moves[move_id], move));
+			// Append move_id, move pp, move max pp, and move status
+			std::map<int, json> move_map;
+			for (json move: pokemon["moveSlots"]){
+				std::string move_id = move["id"];
+				move_map.insert(std::make_pair(moves[move_id], move));
+			}
+			std::map<int, json>::iterator it = move_map.begin();
+			while(it != move_map.end()) {
+				result.push_back(it->first);
+				result.push_back(2 * double((it->second)["pp"]) / double((it->second)["maxpp"]) - 1);
+				result.push_back(2 * int((it->second)["disabled"]) - 1);
+				it++;
+			}
+			// Append status
+			result = addStatusVector(status, pokemon["status"], result);
+			// Append stats
+			result = addNormStats(pokemon["baseStoredStats"], pokemon["hp"], result);
+			// Append stat boosts
+			result = addNormStats(pokemon["boosts"], result);
+			// Append trapped
+			result.push_back(2 * int(pokemon["trapped"]) - 1);
+		}
 	}
-	std::map<int, json>::iterator it = move_map.begin();
-    while(it != move_map.end()) {
-		tmp2.push_back(it->first);
-		tmp2.push_back(double((it->second)["pp"]) / double((it->second)["maxpp"]));
-		tmp2.push_back(int((it->second)["disabled"]));
-        it++;
-    }
-	// Append status
-	tmp2 = addStatusVector(status, state["sides"][0]["pokemon"][0]["status"], tmp2);
-	tmp2 = addNormStats(state["sides"][0]["pokemon"][0]["baseStoredStats"], state["sides"][0]["pokemon"][0]["hp"], tmp2);
-
 	// Print out state vector
-	for (const double& i : tmp2) {
+	for (const double& i : result) {
     	std::cerr << i << "  " << std::endl;
   	}
   return 0;
